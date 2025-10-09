173c533e7b540387600d6a52432b40ebde8d0258
Alar of Runetotem <alar@aspide.it>
2025-10-09 23:30:53 +0200

    Merge remote-tracking branch 'origin/develop'

--- LibInit/LibInit.lua
+++ LibInit/LibInit.lua
@@ -1,7 +1,7 @@
 --- Main methods directly available in your addon
 -- @module lib
 -- @author Alar of Runetotem
--- @release 79
+-- @release 80
 -- @set sort=true
 -- @usage
 -- -- Create a new addon this way:
@@ -15,7 +15,7 @@ local me, ns = ...
 ---@diagnostic disable-next-line: undefined-field
 local __FILE__=tostring(debugstack(1,2,0):match("(.*):16:")) -- Always check line number in regexp and file
 local MAJOR_VERSION = "LibInit"
-local MINOR_VERSION = 79
+local MINOR_VERSION = 80
 local LibStub=LibStub
 local dprint=function(...) end
 local encapsulate  = function ()
